64029e1afcb465af9f070c351b458a47d961a08c
quantscientist <arya.has.fun@gmail.com>
2021-08-03 19:52:19 -0400

    cleaned org chart

--- demo/js/dndtree.js
+++ demo/js/dndtree.js
@@ -411,38 +411,38 @@ treeJSON = d3.json("org_data_1.json", function(error, treeData) {
             .attr("transform", function(d) {
                 return "translate(" + source.y0 + "," + source.x0 + ")";
             })
-            .on('click', click)
-            .on('mouseover', function (d, i) {
-          d3.select(this).transition()
-               .duration('50')
-               .attr('opacity', '.85');
-          div.transition()
-               .duration(50)
-               .style("opacity", 1);
-          var num_children = 0;
-          var childCount = function(num_children,n) {
-
-            if (n.children && n.children.length > 0) {
-                num_children += n.children.length;
-                n.children.forEach(function(num_children,d) {
-                    childCount(num_children,d);
-                });
-            }
-            return num_children;
-        };
-          num = childCount(num_children,d);
-          div.html(num)
-               .style("left", (d3.event.pageX + 10) + "px")
-               .style("top", (d3.event.pageY - 15) + "px");
-     })
-     .on('mouseout', function (d, i) {
-          d3.select(this).transition()
-               .duration('50')
-               .attr('opacity', '1');
-          div.transition()
-               .duration('50')
-               .style("opacity", 0);
-     });
+            .on('click', click);
+          //   .on('mouseover', function (d, i) {
+          // d3.select(this).transition()
+          //      .duration('50')
+          //      .attr('opacity', '.85');
+          // div.transition()
+          //      .duration(50)
+          //      .style("opacity", 1);
+          // var num_children = 0;
+        //   var childCount = function(num_children,n) {
+        //
+        //     if (n.children && n.children.length > 0) {
+        //         num_children += n.children.length;
+        //         n.children.forEach(function(num_children,d) {
+        //             childCount(num_children,d);
+        //         });
+        //     }
+        //     return num_children;
+        // };
+     //      num = childCount(num_children,d);
+     //      div.html(num)
+     //           .style("left", (d3.event.pageX + 10) + "px")
+     //           .style("top", (d3.event.pageY - 15) + "px");
+     // })
+     // .on('mouseout', function (d, i) {
+     //      d3.select(this).transition()
+     //           .duration('50')
+     //           .attr('opacity', '1');
+     //      div.transition()
+     //           .duration('50')
+     //           .style("opacity", 0);
+     // });
 
         nodeEnter.append("circle")
             .attr('class', 'nodeCircle')
